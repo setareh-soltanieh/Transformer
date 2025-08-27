4f9babf50433c52d990aae45c153aab312dae744
setareh-soltanieh <setarehsoltanieh78@gmail.com>
2025-08-27 12:28:09 -0400

    Completed the getitem in the dataset.py

--- dataset.py
+++ dataset.py
@@ -31,6 +31,57 @@ class BilingualDataset(Dataset):
         enc_num_padding_tokens = self.seq_len - len(enc_input_tokens) - 2 # -2 because of SOS and EOS
         dec_num_padding_tokens = self.seq_len - len(dec_input_tokens) - 1 # in the decoder we only use the SOS so only -1
 
+        # if the seq_len that we chose is not long enough to cover all the sentences, we are raising an error
+        if enc_num_padding_tokens < 0 or dec_num_padding_tokens < 0:
+            raise ValueError('Sentence is too long')
+
+        encoder_input = torch.cat(
+            [
+                self.sos_token,
+                torch.tensor(enc_input_tokens, dtype=torch.int64),
+                self.eos_token,
+                torch.tensor([self.pad_token] * enc_num_padding_tokens, dtype=torch.int64)
+            ]
+        )
+
+        decoder_input = torch.cat(
+            [
+                self.sos_token,
+                torch.tensor(dec_input_tokens, dtype=torch.int64),
+                torch.tensor([self.pad_token] * dec_num_padding_tokens, dtype=torch.int64)
+            ]
+        )
+
+        label = torch.cat(
+            [
+                torch.tensor(dec_input_tokens, dtype=torch.int64),
+                self.eos_token,
+                torch.tensor([self.pad_token] * dec_num_padding_tokens, dtype=torch.int64)
+            ]
+        )
+
+        assert encoder_input.size(0) == self.seq_len
+        assert decoder_input.size(0) == self.seq_len
+        assert label.size(0) == self.seq_len
+
+        # we are returning a dictionary of encoder and decoder inputs with the encoder mask that masks
+        # all of the padded tokens which are not usefull.
+        # we use the same thing for the decoder_mask but with one difference that each word only looks at all the previous words
+        return {
+            "encoder_input": encoder_input, # (Seq_len)
+            "decoder_input": decoder_input, # (Seq_len)
+            "encoder_mask": (encoder_input != self.pad_token).unsqueeze(0).unsqueeze(0).int(),  # (1, 1, Seq_len)
+            "decoder_mask": (decoder_input != self.pad_token).unsqueeze(0).unsqueeze(0).int() & causal_mask(decoder_input.size(0)),
+            "label": label,
+            "src_text": src_text,
+            "tgt_text": tgt_text
+        }
+
+def causal_mask(size):
+    # the torch.triu converts all of the elements of the input matrix that are below the diagonal to 0
+    # so here if the mask is equal to 0 we use that encoder input
+    mask = torch.triu(torch.ones(1, size, size), diagonal=1).type(torch.int)
+    return mask == 0
 
 
 
